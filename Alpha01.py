63770afea3303046d644eef66b76a5c27779c6fc
DziGrobZoro <58659189+DziGrobZoro@users.noreply.github.com>
2020-02-27 00:00:36 +0300

    Update Alpha01.py

--- Alpha01.py
+++ Alpha01.py
@@ -10,30 +10,31 @@ class Tokenizer(object):
         result = [] 
         if len(s) == 0:
             result = []
-        # check if obj is/isn`t alpha
-        for a,b in enumerate(s):
+        else:
+            # check if obj is/isn`t alpha
+            for a,b in enumerate(s):
 
+                if b.isalpha():
+                    # the beginning of the substring
+                    # can be the first char in string
+                    # or if not, the first alpha
+                    # that code can find in string
+                    # (previous is not alpha)
+
+                    if a == 0 or not s[a-1].isalpha():
+                        i = a
+                else:
+                    i = a + 1
+                # if it is not the last char in the string
+                if (a+1 <= len(s)-1):
+                    # code finds the end of substring
+                    # and adds it to list
+                    if b.isalpha() and not s[a+1].isalpha():
+                        result.append(s[i:a+1])
+            # code goes till the end of the string
+            # and if the last one is alpha adds it
             if b.isalpha():
-            # the beginning of the substring
-            # can be the first char in string
-            # or if not, the first alpha
-            # that code can find in string
-            # (previous is not alpha)
-
-                if a == 0 or not s[a-1].isalpha():
-                    i = a
-            else:
-                i = a + 1
-            # if it is not the last char in the string
-            if (a+1 <= len(s)-1):
-                # code finds the end of substring
-                # and adds it to list
-                if b.isalpha() and not s[a+1].isalpha():
-                    result.append(s[i:a+1])
-        # code goes till the end of the string
-        # and if the last one is alpha adds it
-        if b.isalpha():
-            result.append(s[i:])
+                result.append(s[i:])
 
         return result
 
