4d5bfc138f2621c4759d260d0b9f97859d269c21
DziGrobZoro <58659189+DziGrobZoro@users.noreply.github.com>
2020-02-26 23:32:30 +0300

    Update Alpha01.py

--- Alpha01.py
+++ Alpha01.py
@@ -1,25 +1,37 @@
+"""
+This module has funtions and methods 
+to tokenize an input string
+"""
 class Tokenizer(object):
-
+    """This class has method to tokenize an input string"""
     def tokenize(self,s):
-    
+    """This function divides a string in a list of alphabetical substrings."""
         i = 0
         result = [] 
         if len(s) == 0:
             result = []
-
+        # check if obj is/isn`t alpha
         for a,b in enumerate(s):
 
             if b.isalpha():
+            # the beginning of the substring
+            # can be the first char in string
+            # or if not, the first alpha
+            # that code can find in string
+            # (previous is not alpha)
+
                 if a == 0 or not s[a-1].isalpha():
                     i = a
             else:
                 i = a + 1
-        
+            # if it is not the last char in the string
             if (a+1 <= len(s)-1):
-        
+                # code finds the end of substring
+                # and adds it to list
                 if b.isalpha() and not s[a+1].isalpha():
                     result.append(s[i:a+1])
-
+        # code goes till the end of the string
+        # and if the last one is alpha adds it
         if b.isalpha():
             result.append(s[i:])
 
